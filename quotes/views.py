eae88f42505f92a3ffe65557c9f7322f82b4ce7a
William De Rocco <wderocco@bu.edu>
2021-11-01 12:58:25 -0400

    FINAL added mini_fb_url.txt

--- quotes/views.py
+++ quotes/views.py
@@ -1,11 +1,17 @@
 from django.db import models
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Quote
 
 
 class HomePageView(ListView):
     '''Show a listing of Quotes.'''
-    model = Quote # retrieve Quote objects from the database
-    template_name = "quotes/home.html"
-    context_object_name = "quotes"
+    model = Quote                       # retrieve Quote objects from the database
+    template_name = "quotes/home.html"  # delegate the display to this template
+    context_object_name = "quotes"      # use this variable name in the template
+
+class QuotePageView(DetailView):
+    '''Display a single quote object.'''
+    model = Quote                       # retrieve Quote objects from the database
+    template_name = "quotes/quote.html" # delegate the display to this template
+    context_object_name = "quote"       # use this variable name in the template

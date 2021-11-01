7196de6af015b12b4d96ad68b42ce6a3776c3a15
William De Rocco <wderocco@bu.edu>
2021-11-01 11:23:25 -0400

    adding mini_fb

---
+++ mini_fb/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class Profile(models.Model):
+    '''Represents profile involving name, hometown, and a profile pic'''
+
+    # data attributes:
+    first_name = models.TextField(blank=True)
+    last_name = models.TextField(blank=True)
+    city = models.TextField(blank=True)
+    email_address = models.TextField(blank=True)
+    image_url = models.URLField(blank=True)
+
+    def __str__(self):
+        '''Return a string representation of profile.'''
+
+        return f'{self.first_name} {self.last_name} {self.city} {self.email_address} {self.image_url}'
